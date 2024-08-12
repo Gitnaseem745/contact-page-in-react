429eea0dbd435c7934be0380d8aa4c90866ca206
Naseem Ansari <mrnaseem745@gmail.com>
2024-08-12 16:32:10 +0530

    #working on form container

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {MdMessage} from "react-icons/md"
 import styles from './Button.module.css'
-const Button = ({btnText, btnIcon}) => {
+const Button = ({btnText, btnIcon, isOutline}) => {
   return (
-    <button className={styles.primaryBtn}>{btnIcon} {btnText}</button>
+    <button className={isOutline ? styles.outlineBtn : styles.primaryBtn}>{btnIcon} {btnText}</button>
   )
 }
 
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import styles from './ContactForm.module.css'
 import Button from '../Button/Button'
-import { MdMessage, MdPhone } from 'react-icons/md'
+import { MdMail, MdMessage, MdPhone } from 'react-icons/md'
 
 const ContactForm = () => {
   return (
     <section className={`${styles.container} container`}>
         <div className={styles.contactForm}>
+            <div className={styles.topBtn}>
             <Button
             btnText="Via Support Chat"
-            btnIcon={<MdMessage />}
+            btnIcon={<MdMessage fontSize="24px"/>}
             />
             <Button
             btnText="Via Call"
-            btnIcon={<MdPhone />}
+            btnIcon={<MdPhone fontSize="24px"/>}
             />
+            </div>
+            <Button
+            isOutline={true}
+            btnText="Via Email Form"
+            btnIcon={<MdMail fontSize="24px"/>}
+            />
+            <form>
+                <div className={styles.formContainer}>
+                <label htmlFor="Name">Name</label>
+                <input type="text" name='name'/>
+                </div>
+            </form>
         </div>
         <div className={styles.contactImg}>
 
